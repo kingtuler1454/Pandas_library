5775f6d214059fa69d0eecb7ff25d7f6f620ab95
kingtuler <morozovsergey.03@gmail.com>
2022-12-12 20:06:11 +0400

    until oform

--- work.py
+++ work.py
@@ -3,21 +3,7 @@ from tqdm import trange
 import pymorphy2
 import matplotlib.pyplot as plt
 
-
 import iterator
-
-
-def read_file(elem: str)->str:
-    """"reading a data from csv"""
-    if elem != "['Абсолютный путь к файлу,Относительный путь к файлу,номер звезды']":
-        directory = str(elem).split(",")
-        #print(len(directory))
-        if len(directory) == 3:
-            with open(directory[1], "r") as f:
-                text = f.read()
-            
-            return text     
-    return None
 
 
 def create_table()-> pd.DataFrame:
@@ -35,11 +21,31 @@ def create_table()-> pd.DataFrame:
     return df
 
 
-def check_table(df)-> pd.DataFrame:
+def read_file(elem: str)->str:
+    """"reading a data from csv"""
+    if elem != "['Абсолютный путь к файлу,Относительный путь к файлу,номер звезды']":
+        directory = str(elem).split(",")
+        #print(len(directory))
+        if len(directory) == 3:
+            with open(directory[1], "r") as f:
+                text = f.read()
+            
+            return text     
+    return None
+
+
+df=create_table()
+
+
+def check_table(df:pd.DataFrame)->None:
     """check table to correct value"""
     print("checking")
-    df.dropna()
+    df=df.dropna()
+    print(df.info())
     return df
+
+
+df=check_table(df)
 
 
 def add_column(df)-> pd.DataFrame:
@@ -53,42 +59,59 @@ def add_column(df)-> pd.DataFrame:
     return df
 
 
-def statistic(df)-> None:
-    """statistic in star"""
-    print(df.groupby('star').count())
-    
+df=add_column(df)
 
-def sorted_table(df,count_words: int)->None:
+def statistic(df: pd.DataFrame)-> None:
+    """statistic in star"""
+    print(df.describe())
+
+
+statistic(df)
+
+
+def sorted_table(df: pd.DataFrame,count_words: int)->None:
     """sorted_tabel on width words"""
     df=df[df['len_text'] > count_words][['star', 'text','len_text']]
-    
+    print(df)
+    return df
 
-def table_star(df, number_star: int)->None:
+sorted_table(df,350)
+
+
+def table_star(df: pd.DataFrame, number_star: int)->None:
     """Sorted table"""
-    df=df[df['star']==number_star]
+    print(df[df['star']==number_star])
+    return df[df['star']==number_star]
 
+    
+df=table_star(df,4)
 
-def table_star_statistic(df,number_star: int)-> None:
+def table_star_statistic(df: pd.DataFrame, number_star: int)-> None:
     """print df where star== number_star"""
     df=df[df['star']==number_star]
     print(df)
     print("кол-во слов:\nmin: " + str(df["len_text"].min()) + "\nmean: " + str(df["len_text"].mean()) 
     + "\nmax: " + str(df["len_text"].max()) )
 
+table_star_statistic(df,4)
 
-def mnld(df)-> list:
+
+def create_hist(df : pd.DataFrame)-> list:
     """Lemitization and counter words"""
-    len_words = [0]*20
+    #df=df[df['star']==number_star]
+    words = []
     morph = pymorphy2.MorphAnalyzer()
     print("Обрабатываем кол-во слов")
     for i in trange(len(df['text'])):
-        words = df.text[i].split() # разбиваем текст на слова
+        df.text[i].split() # разбиваем текст на слова
         for word in words:
             p = morph.parse(word)[0]
-            if len(p.normal_form) < len(len_words):
-                len_words[len(p.normal_form)-1]+=1
-    return len_words
+            words.append(p.normal_form)
+    lonely_words=list(set(words))
+    print(lonely_words)    
+    #return words
 
+create_hist(df)
 
 def print_histogram(len_words: list)->None:
     """print histogram on based list"""
@@ -102,3 +125,5 @@ def print_histogram(len_words: list)->None:
     plt.xlim(0)
     plt.ylim(0)
     plt.show()
+
+print_histogram(len_words)
