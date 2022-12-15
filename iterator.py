6a356ac0760efcde736186680500dc4aa6f401cd
Sergey <108353373+kingtuler1454@users.noreply.github.com>
2022-12-15 11:58:39 +0400

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -3,6 +3,7 @@ import re
 import csv
 from typing import Optional
 
+
 class Iterator:
     def __init__(self, way_to_csv_file: str, name_class: str):
         self.name_class = str(name_class)
@@ -13,13 +14,11 @@ class Iterator:
         file = open(self.way_to_file, "r", encoding="utf-8")
         reader = csv.reader(file, delimiter="\t")
         for row in reader:
-            if str(row)[-3]==self.name_class:
+            if str(row)[-3] == self.name_class:
                 self.list.append(row)
-
 
     def __iter__(self):
         return self
-
 
     def __next__(self):
         if self.counter < len(self.list):
